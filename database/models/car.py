7bc1bbb60ee5d820ef8647542cfbc440f04e9367
Svoevolin <vanish@internet.ru>
2023-11-13 15:58:14 +0300

    modify models again according real task

--- database/models/car.py
+++ database/models/car.py
@@ -7,10 +7,11 @@ class Car(Base):
     __tablename__ = "car"
 
     id_car = mapped_column(Integer, primary_key=True, autoincrement=True)
-    id_performance_characteristic = mapped_column(ForeignKey(
-        'car_performance_characteristic.id_performance_characteristic'), nullable=False)
     id_colour = mapped_column(ForeignKey('car_colour.id_colour'), nullable=False)
     id_condition = mapped_column(ForeignKey('car_condition.id_condition'), nullable=False)
+    id_model = mapped_column(ForeignKey('car_model.id_model'), nullable=False)
+    id_engine = mapped_column(ForeignKey('car_engine.id_engine'), nullable=False)
+    id_gearbox = mapped_column(ForeignKey('car_gearbox.id_gearbox'), nullable=False)
     year = mapped_column(SmallInteger, nullable=False)
     mileage = mapped_column(Integer, nullable=False)
     price = mapped_column(Integer, nullable=False)
